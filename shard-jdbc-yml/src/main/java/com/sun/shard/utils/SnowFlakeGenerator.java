598350ad481686f4d9205bf90d499ec8b9678408
sfyer <1273707178@qq.com>
2020-05-27 18:07:53 +0800

    003

--- shard-jdbc-yml/src/main/java/com/sun/shard/utils/SnowFlakeGenerator.java
+++ shard-jdbc-yml/src/main/java/com/sun/shard/utils/SnowFlakeGenerator.java
@@ -1,138 +1,143 @@
 package com.sun.shard.utils;
 
 public class SnowFlakeGenerator {
-	 public static class Factory {
-	        /**
-	         * 每一部分占用位数的默认值
-	         */
-	        private final static int DEFAULT_MACHINE_BIT_NUM = 5;   //机器标识占用的位数
-	        private final static int DEFAULT_IDC_BIT_NUM = 5;//数据中心占用的位数
 
-	        private int machineBitNum;
-	        private int idcBitNum;
+    public static class Factory {
 
-	        public Factory() {
-	            this.idcBitNum = DEFAULT_IDC_BIT_NUM;
-	            this.machineBitNum = DEFAULT_MACHINE_BIT_NUM;
-	        }
+        /**
+         * 每一部分占用位数的默认值
+         */
+        private final static int DEFAULT_MACHINE_BIT_NUM = 5;   //机器标识占用的位数
+        private final static int DEFAULT_IDC_BIT_NUM = 5;//数据中心占用的位数
 
-	        public Factory(int machineBitNum, int idcBitNum) {
-	            this.idcBitNum = idcBitNum;
-	            this.machineBitNum = machineBitNum;
-	        }
+        private int machineBitNum;
+        private int idcBitNum;
 
-	        public SnowFlakeGenerator create(long idcId, long machineId) {
-	            return new SnowFlakeGenerator(this.idcBitNum, this.machineBitNum, idcId, machineId);
-	        }
-	    }
+        public Factory() {
+            this.idcBitNum = DEFAULT_IDC_BIT_NUM;
+            this.machineBitNum = DEFAULT_MACHINE_BIT_NUM;
+        }
 
-	    /**
-	     * 起始的时间戳
-	     * 作者写代码时的时间戳
-	     */
-	    private final static long START_STAMP = 1508143349995L;
+        public Factory(int machineBitNum, int idcBitNum) {
+            this.idcBitNum = idcBitNum;
+            this.machineBitNum = machineBitNum;
+        }
 
-	    /**
-	     * 可分配的位数
-	     */
-	    private final static int REMAIN_BIT_NUM = 22;
+        public SnowFlakeGenerator create(long idcId, long machineId) {
+            return new SnowFlakeGenerator(this.idcBitNum, this.machineBitNum, idcId, machineId);
+        }
+    }
 
-	    /**
-	     * idc编号
-	     */
-	    private long idcId;
+    /**
+     * 起始的时间戳
+     * 作者写代码时的时间戳
+     */
+    private final static long START_STAMP = 1508143349995L;
 
-	    /**
-	     * 机器编号
-	     */
-	    private long machineId;
+    /**
+     * 可分配的位数
+     */
+    private final static int REMAIN_BIT_NUM = 22;
 
-	    /**
-	     * 当前序列号
-	     */
-	    private long sequence = 0L;
+    /**
+     * idc编号
+     */
+    private long idcId;
 
-	    /**
-	     * 上次最新时间戳
-	     */
-	    private long lastStamp = -1L;
+    /**
+     * 机器编号
+     */
+    private long machineId;
 
-	    /**
-	     * idc偏移量：一次计算出，避免重复计算
-	     */
-	    private int idcBitLeftOffset;
+    /**
+     * 当前序列号
+     */
+    private long sequence = 0L;
 
-	    /**
-	     * 机器id偏移量：一次计算出，避免重复计算
-	     */
-	    private int machineBitLeftOffset;
+    /**
+     * 上次最新时间戳
+     */
+    private long lastStamp = -1L;
 
-	    /**
-	     * 时间戳偏移量：一次计算出，避免重复计算
-	     */
-	    private int timestampBitLeftOffset;
+    /**
+     * idc偏移量：一次计算出，避免重复计算
+     */
+    private int idcBitLeftOffset;
 
-	    /**
-	     * 最大序列值：一次计算出，避免重复计算
-	     */
-	    private int maxSequenceValue;
+    /**
+     * 机器id偏移量：一次计算出，避免重复计算
+     */
+    private int machineBitLeftOffset;
 
-	    private SnowFlakeGenerator(int idcBitNum, int machineBitNum, long idcId, long machineId) {
-	        int sequenceBitNum = REMAIN_BIT_NUM - idcBitNum - machineBitNum;
+    /**
+     * 时间戳偏移量：一次计算出，避免重复计算
+     */
+    private int timestampBitLeftOffset;
 
-	        if (idcBitNum <= 0 || machineBitNum <= 0 || sequenceBitNum <= 0) {
-	            throw new IllegalArgumentException("error bit number");
-	        }
+    /**
+     * 最大序列值：一次计算出，避免重复计算
+     */
+    private int maxSequenceValue;
 
-	        this.maxSequenceValue = ~(-1 << sequenceBitNum);
+    private SnowFlakeGenerator(int idcBitNum, int machineBitNum, long idcId, long machineId) {
+        int sequenceBitNum = REMAIN_BIT_NUM - idcBitNum - machineBitNum;
 
-	        machineBitLeftOffset = sequenceBitNum;
-	        idcBitLeftOffset = idcBitNum + sequenceBitNum;
-	        timestampBitLeftOffset = idcBitNum + machineBitNum + sequenceBitNum;
+        if (idcBitNum <= 0 || machineBitNum <= 0 || sequenceBitNum <= 0) {
+            throw new IllegalArgumentException("error bit number");
+        }
 
-	        this.idcId = idcId;
-	        this.machineId = machineId;
-	    }
+        this.maxSequenceValue = ~(-1 << sequenceBitNum);
 
-	    /**
-	     * 产生下一个ID
-	     */
-	    public synchronized long nextId() {
-	        long currentStamp = getTimeMill();
-	        if (currentStamp < lastStamp) {
-	            throw new RuntimeException(String.format("Clock moved backwards. Refusing to generate id for %d milliseconds", lastStamp - currentStamp));
-	        }
+        machineBitLeftOffset = sequenceBitNum;
+        idcBitLeftOffset = idcBitNum + sequenceBitNum;
+        timestampBitLeftOffset = idcBitNum + machineBitNum + sequenceBitNum;
 
-	        //新的毫秒，序列从0开始，否则序列自增
-	        if (currentStamp == lastStamp) {
-	            sequence = (sequence + 1) & this.maxSequenceValue;
-	            if (sequence == 0L) {
-	                //Twitter源代码中的逻辑是循环，直到下一个毫秒
-	                lastStamp = tilNextMillis();
+        this.idcId = idcId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * 产生下一个ID
+     */
+    public synchronized long nextId() {
+        long currentStamp = getTimeMill();
+        if (currentStamp < lastStamp) {
+            throw new RuntimeException(String
+                .format("Clock moved backwards. Refusing to generate id for %d milliseconds",
+                    lastStamp - currentStamp));
+        }
+
+        //新的毫秒，序列从0开始，否则序列自增
+        if (currentStamp == lastStamp) {
+            sequence = (sequence + 1) & this.maxSequenceValue;
+            if (sequence == 0L) {
+                //Twitter源代码中的逻辑是循环，直到下一个毫秒
+                lastStamp = tilNextMillis();
 //	                throw new IllegalStateException("sequence over flow");
-	            }
-	        } else {
-	            sequence = 0L;
-	        }
+            }
+        } else {
+            sequence = 0L;
+        }
 
-	        lastStamp = currentStamp;
+        lastStamp = currentStamp;
 
-	        return (currentStamp - START_STAMP) << timestampBitLeftOffset | idcId << idcBitLeftOffset | machineId << machineBitLeftOffset | sequence;
-	    }
+        return (currentStamp - START_STAMP) << timestampBitLeftOffset | idcId << idcBitLeftOffset
+            | machineId << machineBitLeftOffset | sequence;
+    }
 
-	    private long getTimeMill() {
-	        return System.currentTimeMillis();
-	    }
+    private long getTimeMill() {
+        return System.currentTimeMillis();
+    }
 
-	    private long tilNextMillis() {
-	        long timestamp = getTimeMill();
-	        while (timestamp <= lastStamp) {
-	            timestamp = getTimeMill();
-	        }
-	        return timestamp;
-	    }
-	    
-	    public static void main(String[] args) {
-			System.out.println(new SnowFlakeGenerator.Factory().create(5, 5).nextId());
-		}
+    private long tilNextMillis() {
+        long timestamp = getTimeMill();
+        while (timestamp <= lastStamp) {
+            timestamp = getTimeMill();
+        }
+        return timestamp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SnowFlakeGenerator.Factory().create(5, 5).nextId());
+    }
 }
