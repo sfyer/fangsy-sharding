a9c8f1278287ba7f7587f6efe39ae47a75caafa7
sfyer <1273707178@qq.com>
2020-05-27 17:28:22 +0800

    删除

---
+++ src/main/java/com/fsy/sharding/config/ShardingConfig.java
@@ -0,0 +1,16 @@
+package com.fsy.sharding.config;
+
+/**
+ * @Title: ShardingConfig
+ * @Description:
+ * @Team: 财务产品研发部
+ * @Author fangsy
+ * @Date 2020-05-27
+ */
+public class ShardingConfig {
+
+    
+
+
+
+}
---
+++ src/main/java/com/fsy/sharding/entity/ShardingTestUser.java
@@ -0,0 +1,19 @@
+package com.fsy.sharding.entity;
+
+/**
+ * @Title: ShardingTestOrder
+ * @Description:
+ * @Team: 财务产品研发部
+ * @Author fangsy
+ * @Date 2020-05-27
+ */
+public class ShardingTestUser {
+
+    private Long id;
+
+    private Sting
+
+
+
+
+}
